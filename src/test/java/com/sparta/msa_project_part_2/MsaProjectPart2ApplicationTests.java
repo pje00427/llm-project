8d64642035193a5bf1152cf7308b95ad6098cd98
pje00427 <pje00427@naver.com>
2026-03-28 17:35:26 +0900

    spring init

---
+++ src/test/java/com/sparta/msa_project_part_2/MsaProjectPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.msa_project_part_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaProjectPart2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
